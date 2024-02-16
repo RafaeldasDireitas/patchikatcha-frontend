bd4d8cc87928e0347065e1626b2bbc56788f7065
Rafael <vaizard500@gmail.com>
2024-02-16 23:49:01 +0000

    added navbar and API fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
 "use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return <button className="btn">Autentication</button>;
+   const [dataE, setData] = useState<any>();
+
+   useEffect(() => {
+      const fetchData = async () => {
+         const data = await fetch("https://localhost:7185/api/Mug", {
+            method: "GET"
+         });
+
+         const jsonData = await data.json();
+         setData(jsonData);
+      };
+
+      fetchData();
+   }, []);
+
+   console.log(dataE);
+
+   return <>{dataE && <h1>{dataE[0].name}</h1>}</>;
 }

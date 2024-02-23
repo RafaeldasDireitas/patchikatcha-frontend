0a5a0ad295dc482c3da3b52f771f2ae4a3a422df
Rafael <vaizard500@gmail.com>
2024-02-23 13:48:38 +0000

    cleaned the code a bit

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -3,31 +3,31 @@ export default function Footer() {
       <>
          <footer className="bg-button-background">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-               <span className="text-sm text-black sm:text-center justify-center flex">
+               <span className="text-sm text-white sm:text-center justify-center flex">
                   © 2024{" "}
-                  <a href="https://flowbite.com/" className="hover:underline">
+                  <a href="https://flowbite.com/" className="hover:underline text-white">
                      PatchiKatcha™
                   </a>
                   . All Rights Reserved.
                </span>
                <ul className="flex flex-wrap items-center justify-center mt-3 text-sm font-medium text-black sm:mt-0">
                   <li>
-                     <a href="#" className=" hover:underline me-4 md:me-6">
+                     <a href="#" className=" hover:underline me-4 md:me-6 text-white">
                         About
                      </a>
                   </li>
                   <li>
-                     <a href="#" className="hover:underline me-4 md:me-6">
+                     <a href="#" className="hover:underline me-4 md:me-6 text-white">
                         Privacy Policy
                      </a>
                   </li>
                   <li>
-                     <a href="#" className="hover:underline me-4 md:me-6">
+                     <a href="#" className="hover:underline me-4 md:me-6 text-white">
                         Licensing
                      </a>
                   </li>
                   <li>
-                     <a href="#" className="hover:underline">
+                     <a href="#" className="hover:underline text-white">
                         Contact
                      </a>
                   </li>
--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,10 @@ import Hero from "./Hero";
 import NewProducts from "./NewProducts";
 
 export default function Home() {
-   const sendEmail = async () => {
-      const email = await fetch("/api/send-email-verification", {
-         method: "POST",
-         body: JSON.stringify("lol")
-      });
-   };
-
    return (
       <>
          <Hero />
          <NewProducts />
-         <button className="btn" onClick={sendEmail}>
-            Send Email
-         </button>
       </>
    );
 }

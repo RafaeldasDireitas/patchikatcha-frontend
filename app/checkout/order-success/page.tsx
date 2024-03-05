b23aabe0ac6d0041d32eb1c6ee3c1ba2f5136ca7
Rafael <vaizard500@gmail.com>
2024-03-05 20:28:03 +0000

    ckeabed cide

---
+++ app/checkout/order-success/page.tsx
@@ -0,0 +1,7 @@
+export default function OrderSucess() {
+   return (
+      <>
+         <h1>Order success</h1>
+      </>
+   );
+}
--- app/components/cartdrawer/CartProduct.tsx
+++ app/components/cartdrawer/CartProduct.tsx
@@ -1,11 +1,8 @@
 "use client";
-import { useGlobalStore } from "@/zustand/globalstore";
 import Image from "next/image";
-import cross from "@/public/cross.png";
 import Link from "next/link";
 
 export default function CartProduct({ name, description, price, price_id, quantity, image, index, product_id }: any) {
-   const globalStore = useGlobalStore();
    const formattedPrice = (price / 100).toFixed(2) + " €";
 
    return (
@@ -14,9 +11,6 @@ export default function CartProduct({ name, description, price, price_id, quanti
             <Link href={{ pathname: `/product/${name}`, query: { productId: product_id } }}>
                <Image src={image} width={100} height={100} alt="Product image"></Image>
             </Link>
-            <button className="btn w-10 bg-button-background border-none py-1" onClick={() => globalStore.removeFromCart(index)}>
-               <Image src={cross} width={10} height={10} alt="Cross"></Image>
-            </button>
          </div>
          <div className="flex flex-col px-2">
             <h1 className="text-lg">{name}</h1>

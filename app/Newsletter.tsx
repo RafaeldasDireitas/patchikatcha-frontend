eb12700e893064ee6c458d511764d2713f29882d
Rafael <vaizard500@gmail.com>
2024-03-30 03:06:56 +0000

    added newsletter

---
+++ app/Newsletter.tsx
@@ -0,0 +1,11 @@
+export default function Newsletter() {
+   return (
+      <>
+         <div className="flex flex-col items-center">
+            <h1>Sign up for our newsletter</h1>
+            <p>Stay up to date with new products and announcements!</p>
+            <input></input>
+         </div>
+      </>
+   );
+}

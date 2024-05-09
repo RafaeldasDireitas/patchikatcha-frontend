a4fccd4558265aa18f571227100c1d0e680df72d
Rafael <vaizard500@gmail.com>
2024-05-10 00:40:23 +0100

    polished footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,48 +1,36 @@
 import Link from "next/link";
+import logo from "@/public/new_logo.svg";
+import Image from "next/image";
+import { FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
    return (
       <>
          <footer className="footer p-10 bg-button-background text-white">
             <aside>
-               <svg
-                  width="50"
-                  height="50"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  className="fill-current"
-               >
-                  <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-               </svg>
-               <p>
-                  ACME Industries Ltd.
-                  <br />
-                  Providing reliable tech since 1992
-               </p>
+               <div className="bg-white rounded-xl">
+                  <Image src={logo} width={200} height={200} alt="No logo found"></Image>
+               </div>
+               <p className="quicksand-bold">Copyright © 2024 - All rights reserved</p>
             </aside>
             <nav>
-               <h6 className="footer-title z-0">Services</h6>
-               <a className="link link-hover">Branding</a>
-               <a className="link link-hover">Design</a>
-               <a className="link link-hover">Marketing</a>
-               <a className="link link-hover">Advertisement</a>
+               <h6 className="footer-title opacity-100 z-0 quicksand-bold">Socials</h6>
+               <div className="flex flex-row items-center">
+                  <a className="link link-hover quicksand-medium">Instagram</a>
+                  <FaInstagram color="white" className="mx-2" size={20}></FaInstagram>
+               </div>
             </nav>
             <nav>
-               <h6 className="footer-title z-0">Company</h6>
-               <a className="link link-hover">About us</a>
-               <a className="link link-hover">Contact</a>
-               <a className="link link-hover">Jobs</a>
-               <a className="link link-hover">Press kit</a>
+               <h6 className="footer-title opacity-100 z-0 quicksand-bold">Company</h6>
+               <a className="link link-hover quicksand-medium">About us</a>
+               <a className="link link-hover quicksand-medium">Contacts</a>
             </nav>
             <nav>
-               <h6 className="footer-title z-0">Legal</h6>
+               <h6 className="footer-title opacity-100 z-0 quicksand-bold">Legal</h6>
                <Link href={"/terms-and-services"}>
-                  <p className="link link-hover">Terms and Services</p>
+                  <p className="link link-hover quicksand-medium">Terms and Services</p>
                </Link>
-               <a className="link link-hover">Privacy policy</a>
-               <a className="link link-hover">Cookie policy</a>
+               <a className="link link-hover quicksand-medium">Refund policy</a>
             </nav>
          </footer>
       </>

b195545bd8c93ae10b7b46968302a908f5787471
Wilson Gomes <wilson.gomes@innowave.tech>
2024-05-11 01:46:45 +0000

    Refactoring

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -5,8 +5,6 @@ import { useEffect, useState } from "react";
 import { useGlobalStore } from "@/zustand/globalstore";
 import { CartType } from "@/types/CartType";
 import FetchCreateCheckoutSession from "./FetchCreateCheckoutSession";
-import FetchSessionStatus from "./FetchSessionStatus";
-import { UserDataType } from "@/types/UserDataType";
 import IsNotAuthenticated from "../components/IsNotAuthenticated";
 import FetchIsEmailConfirmed from "../profile/FetchIsEmailConfirmed";
 
@@ -16,36 +14,11 @@ export default function Checkout() {
    const [clientSecret, setClientSecret] = useState("");
    const [clientId, setClientId] = useState("");
    const globalStore = useGlobalStore();
-   const userEmail = globalStore.userEmail;
    const userId = globalStore.userId;
    const jwtToken = globalStore.jwtToken;
-   const userGeo: UserDataType = globalStore.userGeo;
    const cart: CartType[] = globalStore.cart;
    const [isEmailConfirmed, setIsEmailConfirmed] = useState(false);
 
-   const checkoutObject = cart.map((product) => ({
-      index: product.index,
-      name: product.name,
-      description: product.description,
-      price: product.price,
-      priceId: product.price_id,
-      image: product.image,
-      quantity: product.quantity,
-      productId: product.product_id,
-      variantId: product.variant_id,
-      firstItem: product.first_item,
-      additionalItems: product.additional_items
-   }));
-
-   if (!cart) {
-      return (
-         <div>
-            <div className="flex min-h-screen items-center justify-center">
-               <h1>You must have items in your cart!</h1>
-            </div>
-         </div>
-      );
-   }
    useEffect(() => {
       if (userId) {
          FetchIsEmailConfirmed({ userId, setIsEmailConfirmed });
@@ -58,19 +31,29 @@ export default function Checkout() {
       }
    }, []);
 
+   if (!cart) {
+      return (
+         <div>
+            <div className="flex min-h-screen items-center justify-center">
+               <h1>You must have items in your cart!</h1>
+            </div>
+         </div>
+      );
+   }
+
    if (!isEmailConfirmed) {
       return <IsNotAuthenticated />;
    }
 
    return (
-      <>
-         <div id="checkout">
+      <div className="flex justify-center py-10">
+         <div id="checkout" className="container">
             {clientSecret && (
                <EmbeddedCheckoutProvider stripe={stripePromise} options={{ clientSecret }}>
                   <EmbeddedCheckout />
                </EmbeddedCheckoutProvider>
             )}
          </div>
-      </>
+      </div>
    );
 }

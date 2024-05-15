1035650651ea0a0736dedc558a6d5c3f7b197b0e
Rafael <vaizard500@gmail.com>
2024-05-15 10:38:14 +0100

    fixed detailed description area in product page

--- app/product/[productName]/components/ImageDescription.tsx
+++ app/product/[productName]/components/ImageDescription.tsx
@@ -5,7 +5,7 @@ export default function ImageDescription({ product }: any) {
    const lastImage = product.images[product.images.length - 1];
 
    return (
-      <div className="flex flex-1 w-full justify-center">
+      <div className="flex flex-1 w-full justify-center lg:w-1/3">
          <Image className="rounded-xl max-h-[380px]" src={lastImage.src} width={380} height={380} alt="No image found" />
       </div>
    );

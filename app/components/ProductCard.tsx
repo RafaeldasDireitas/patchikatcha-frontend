6dad5cd0ea0ebc83d53f45fc2af6e58f5086f37f
Rafael <vaizard500@gmail.com>
2024-05-27 09:58:53 +0100

    polished fonts

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -15,8 +15,8 @@ export default function ProductCard({ title, price, image }: any) {
    return (
       <div className="flex flex-col justify-center items-center rounded-xl lg:hover:scale-110 hover:cursor-pointer duration-200">
          <Image className="rounded-xl" src={imageUrl} width={275} height={275} alt="Product image" />
-         <h1 className="text-center quicksand pt-3 text-xl  text-light truncate w-80">{title}</h1>
-         <h2 className="text-center quicksand pb-2 ">{formattedPrice}</h2>
+         <h1 className="text-center pt-3 text-xl text-light truncate w-80">{title}</h1>
+         <h2 className="text-center pb-2 ">{formattedPrice}</h2>
       </div>
    );
 }
